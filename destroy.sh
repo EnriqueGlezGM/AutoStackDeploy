67e9b2b8361129a4f261e9a9ff5a64ef0e4a5189
ENRIQUE GONZALEZ MACIAS <e.gmacias@l102.lab.dit.upm.es>
2024-11-01 15:54:28 +0100

    destroy con limpieza

--- destroy.sh
+++ destroy.sh
@@ -1,3 +1,6 @@
 cd /mnt/tmp/openstack_lab-antelope_4n_classic_ovs-v04
 sudo vnx -f openstack_lab.xml -v --destroy
 sudo vnx -f openstack_lab-terraform.xml -v --destroy
+cd /mnt/tmp
+sudo vnx --clean-host
+
